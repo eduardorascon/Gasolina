971fd77f72c80b9d8a632d4b407a1c9137cddf97
eduardorascon <eduardo.rascon@gmail.com>
2017-01-11 23:22:06 -0600

    Button added, this only gets the size of the ArrayList().

--- app/src/main/java/com/eduardorascon/gasolina/gasolina/MainActivity.java
+++ app/src/main/java/com/eduardorascon/gasolina/gasolina/MainActivity.java
@@ -2,6 +2,11 @@ package com.eduardorascon.gasolina.gasolina;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void readCSVFile(View view) {
+        List<String> csv_items = new ArrayList<String>();
+        csv_items.add(0,"Fuuu");
+        csv_items.add(1,"XYZ");
+
+        TextView x = (TextView) findViewById(R.id.textView);
+        x.setText("" + csv_items.size());
     }
 }
