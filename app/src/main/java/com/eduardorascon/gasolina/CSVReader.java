c5ea92542f6db5940e5985236e9c3f6ec30483a0
eduardorascon <eduardo.rascon@gmail.com>
2017-01-14 15:06:22 -0600

    Now we can read the csv file.

--- app/src/main/java/com/eduardorascon/gasolina/CSVReader.java
+++ app/src/main/java/com/eduardorascon/gasolina/CSVReader.java
@@ -1,4 +1,40 @@
 package com.eduardorascon.gasolina;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CSVReader {
+    InputStream inputStream = null;
+
+    public CSVReader(InputStream is) {
+        this.inputStream = is;
+    }
+
+    public List<String[]> read(){
+        List<String[]> resultList = new ArrayList<String[]>();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String csvLine;
+        try {
+            while ((csvLine = bufferedReader.readLine()) != null) {
+                String[] row = csvLine.split(",");
+                resultList.add(row);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error al leer el archivo. " + e);
+        }
+        finally {
+            try {
+                bufferedReader.close();
+            } catch (IOException e) {
+                throw new RuntimeException("Error al cerrar lector." +e);
+            }
+        }
+
+        return resultList;
+    }
 }
