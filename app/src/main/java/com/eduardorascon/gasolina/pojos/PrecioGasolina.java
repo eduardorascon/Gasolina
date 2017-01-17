07cf19105692d2c0dd49382afa93779c06001d6e
eduardorascon <eduardo.rascon@gmail.com>
2017-01-16 23:22:09 -0600

    Getters and Setters added.

--- app/src/main/java/com/eduardorascon/gasolina/pojos/PrecioGasolina.java
+++ app/src/main/java/com/eduardorascon/gasolina/pojos/PrecioGasolina.java
@@ -1,4 +1,25 @@
 package com.eduardorascon.gasolina.pojos;
 
 public class PrecioGasolina {
+    private String nombreDeRegion;
+    private int numeroDeRegion;
+
+    public PrecioGasolina() {
+    }
+
+    public String getNombreDeRegion() {
+        return nombreDeRegion;
+    }
+
+    public void setNombreDeRegion(String nombreDeRegion) {
+        this.nombreDeRegion = nombreDeRegion;
+    }
+
+    public int getNumeroDeRegion() {
+        return numeroDeRegion;
+    }
+
+    public void setNumeroDeRegion(int numeroDeRegion) {
+        this.numeroDeRegion = numeroDeRegion;
+    }
 }
