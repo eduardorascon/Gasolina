4e522584d7ea7146171c69e85d7fcce8f6d9bf66
eduardorascon <eduardo.rascon@gmail.com>
2017-01-19 22:07:56 -0600

    SearchView.OnQueryTextListener implemented.

--- app/src/main/java/com/eduardorascon/gasolina/MainActivity.java
+++ app/src/main/java/com/eduardorascon/gasolina/MainActivity.java
@@ -3,13 +3,16 @@ package com.eduardorascon.gasolina;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.renderscript.ScriptGroup;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.TypedValue;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -21,7 +24,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private List<PrecioGasolina> precioGasolinas = new ArrayList<>();
     private RecyclerView recyclerView;
@@ -58,6 +61,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+
         return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
