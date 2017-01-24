349d36a8335ae0c8d59d54681f643da50bf3a09f
eduardorascon <eduardo.rascon@gmail.com>
2017-01-23 18:55:27 -0600

    Error fixing.

--- app/src/main/java/com/eduardorascon/gasolina/SplashScreenActivity.java
+++ app/src/main/java/com/eduardorascon/gasolina/SplashScreenActivity.java
@@ -1,8 +1,12 @@
 package com.eduardorascon.gasolina;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 public class SplashScreenActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Intent intent = new Intent(this, MainActivity.class);
