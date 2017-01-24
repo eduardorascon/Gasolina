c93e8de892f91b6809540a784173b08d6c4e92e0
eduardorascon <eduardo.rascon@gmail.com>
2017-01-24 00:02:07 -0600

    SplashScreen finished.

--- app/src/main/java/com/eduardorascon/gasolina/SplashScreenActivity.java
+++ app/src/main/java/com/eduardorascon/gasolina/SplashScreenActivity.java
@@ -2,15 +2,29 @@ package com.eduardorascon.gasolina;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME);
     }
 }
