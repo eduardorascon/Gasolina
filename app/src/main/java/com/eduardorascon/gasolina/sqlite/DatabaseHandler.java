ab3297a2bc1283498a745e5eb9d07b3095ce7c83
eduardorascon <eduardo.rascon@gmail.com>
2017-01-29 13:01:09 -0600

    SQLiteOpenHelper extended.

--- app/src/main/java/com/eduardorascon/gasolina/sqlite/DatabaseHandler.java
+++ app/src/main/java/com/eduardorascon/gasolina/sqlite/DatabaseHandler.java
@@ -1,4 +1,24 @@
 package com.eduardorascon.gasolina.sqlite;
 
-public class DatabaseHandler {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private final static String DATABASE_NAME ="preciocombustibles";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
