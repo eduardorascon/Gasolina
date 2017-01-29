d9f661af3ee0da43665bf2289790a4722e68c3a3
eduardorascon <eduardo.rascon@gmail.com>
2017-01-29 12:53:00 -0600

    Properties, getters and setters added.

--- app/src/main/java/com/eduardorascon/gasolina/sqlite/Municipio.java
+++ app/src/main/java/com/eduardorascon/gasolina/sqlite/Municipio.java
@@ -1,4 +1,48 @@
 package com.eduardorascon.gasolina.sqlite;
 
 public class Municipio {
+    private int id;
+    private String municipio, estado, region;
+
+    public Municipio(){
+    }
+
+    public Municipio(int id, String municipio, String estado, String region) {
+        this.id = id;
+        this.municipio = municipio;
+        this.estado = estado;
+        this.region = region;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
