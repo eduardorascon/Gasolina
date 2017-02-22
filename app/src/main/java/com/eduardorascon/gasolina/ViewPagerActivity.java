dbd795c1620722528906377ae3b0401dde10c5ac
eduardorascon <eduardo.rascon@gmail.com>
2017-02-21 22:40:39 -0600

    Error fixing.

--- app/src/main/java/com/eduardorascon/gasolina/ViewPagerActivity.java
+++ app/src/main/java/com/eduardorascon/gasolina/ViewPagerActivity.java
@@ -1,11 +1,21 @@
-public class ViewPagerActivity extends AppCompatActivity{
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+package com.eduardorascon.gasolina;
 
-		ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-		viewPager.setAdapter(new PagerAdapter(this));
-	}
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.eduardorascon.gasolina.R;
+import com.eduardorascon.gasolina.adapters.CustomPagerAdapter;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+    }
 }
---
+++ app/src/main/java/com/eduardorascon/gasolina/adapters/CustomPagerEnum.java
@@ -0,0 +1,24 @@
+package com.eduardorascon.gasolina.adapters;
+
+import com.eduardorascon.gasolina.R;
+
+public enum CustomPagerEnum {
+
+    RED(1, R.layout.activity_main),
+    BLUE(2, R.layout.activity_main);
+
+    private int mTitleResId, mLayoutResId;
+
+    CustomPagerEnum(int titleResId, int layoutResId) {
+        mTitleResId = titleResId;
+        mLayoutResId = layoutResId;
+    }
+
+    public int getTitleResId() {
+        return mTitleResId;
+    }
+
+    public int getLayoutResId() {
+        return mLayoutResId;
+    }
+}
