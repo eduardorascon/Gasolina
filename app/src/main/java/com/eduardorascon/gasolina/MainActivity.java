c4294e2a60148ebb26c172c86623596cdb810027
Eduardo Rascon <eduardo.rascon@gmail.com>
2017-09-10 22:10:23 -0500

    Firebase database changed to a presenter.

--- app/src/main/java/com/eduardorascon/gasolina/MainActivity.java
+++ app/src/main/java/com/eduardorascon/gasolina/MainActivity.java
@@ -3,67 +3,37 @@ package com.eduardorascon.gasolina;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity implements MainActivityView {
+
+    private MainActivityPresenter presenter;
+    private ListView listView;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        presenter.loadStates();
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MainActivityPresenter presenter = new MainActivityPresenter(this, null);
-
-        final ListView listView = (ListView) findViewById(R.id.listView);
-
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1);
-
-        listView.setAdapter(adapter);
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-        final DatabaseReference myRef = database.getReference("estados");
-
-        myRef.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                String value = dataSnapshot.getKey();
-                adapter.add(value);
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.w("TAG:", "Failed to read value.", databaseError.toException());
-            }
-        });
+        presenter = new MainActivityPresenter(this, null);
+        listView = findViewById(R.id.listView);
     }
 
     @Override
     public void displayStates(List<String> stateList) {
-
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1);
+        adapter.addAll(stateList);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/eduardorascon/gasolina/MainActivityPresenter.java
+++ app/src/main/java/com/eduardorascon/gasolina/MainActivityPresenter.java
@@ -1,21 +1,46 @@
 package com.eduardorascon.gasolina;
 
 import com.eduardorascon.gasolina.repositories.StatesRepository;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivityPresenter {
     private MainActivityView view;
     private StatesRepository statesRepository;
+    private List<String> stateList = new ArrayList();
 
     public MainActivityPresenter(MainActivityView view, StatesRepository statesRepository) {
-
         this.view = view;
         this.statesRepository = statesRepository;
     }
 
     public void loadStates() {
-        List<String> stateList = statesRepository.getStateList();
-        view.displayStates(stateList);
+        //List<String> stateList = statesRepository.getStateList();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("estados");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                stateList.clear();
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    String value = postSnapshot.getKey();
+                    stateList.add(value);
+                }
+
+                view.displayStates(stateList);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
