e7d7bb34e7db7b4b4c2e07058d6dd258e0b90dbd
Eduardo Rascon <eduardo.rascon@gmail.com>
2017-09-12 23:11:21 -0500

    First try implementing a RecyclerView.

--- app/src/main/java/com/eduardorascon/gasolina/MainActivity.java
+++ app/src/main/java/com/eduardorascon/gasolina/MainActivity.java
@@ -1,21 +1,26 @@
 package com.eduardorascon.gasolina;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
-
 
 public class MainActivity extends AppCompatActivity implements MainActivityView {
 
     private MainActivityPresenter presenter;
-    private ListView listView;
+    private RecyclerView rv;
+    private LinearLayoutManager llm;
+    private StatesAdapter adapter;
 
-    @Override
     protected void onStart() {
         super.onStart();
         presenter.loadStates();
@@ -27,20 +32,50 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
         setContentView(R.layout.activity_main);
 
         presenter = new MainActivityPresenter(this, null);
-        listView = findViewById(R.id.listView);
+        rv = (RecyclerView) findViewById(R.id.states_recycler_view);
+        rv.setHasFixedSize(true);
+        llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
     }
 
     @Override
     public void displayStates(List<String> stateList) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1);
-        adapter.addAll(stateList);
-        listView.setAdapter(adapter);
+        adapter = new StatesAdapter((ArrayList<String>) stateList);
+        rv.setAdapter(adapter);
     }
 
-    @Override
-    public void showCitiesActivity() {
-        Intent intent = new Intent(this, CitiesActivity.class);
-        startActivity(intent);
+    public class StatesAdapter extends RecyclerView.Adapter<StatesAdapter.ViewHolder> {
+        private ArrayList<String> statesList;
+
+        public StatesAdapter(ArrayList<String> statesList) {
+            this.statesList = statesList;
+        }
+
+        @Override
+        public StatesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.recyclerview_item_row, parent, false);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(StatesAdapter.ViewHolder holder, int position) {
+            holder.mTextView.setText(statesList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return statesList.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView mTextView;
+
+            public ViewHolder(TextView v) {
+                super(v);
+                mTextView = v;
+            }
+        }
     }
 }
