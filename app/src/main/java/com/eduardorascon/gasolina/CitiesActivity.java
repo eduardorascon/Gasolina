21d7fccd08b587d54d448d2b91752bac04cc0163
Eduardo Rascon <eduardo.rascon@gmail.com>
2017-09-14 13:24:47 -0500

    Floating Action Button deleted.

--- app/src/main/java/com/eduardorascon/gasolina/CitiesActivity.java
+++ app/src/main/java/com/eduardorascon/gasolina/CitiesActivity.java
@@ -1,11 +1,8 @@
 package com.eduardorascon.gasolina;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class CitiesActivity extends AppCompatActivity {
 
@@ -15,14 +12,5 @@ public class CitiesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cities);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
