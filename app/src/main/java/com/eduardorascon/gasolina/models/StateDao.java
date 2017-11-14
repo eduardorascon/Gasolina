cacf95e2e431b520f552c77cb4723328d8270393
eduardorascon <eduardo.rascon@gmail.com>
2017-11-13 18:46:42 -0600

    Annotations added to Dao.

--- app/src/main/java/com/eduardorascon/gasolina/models/StateDao.java
+++ app/src/main/java/com/eduardorascon/gasolina/models/StateDao.java
@@ -1,4 +1,13 @@
 package com.eduardorascon.gasolina.models;
 
+@Dao
 public interface StateDao {
+	@Query("SELECT * FROM estados");
+	List<State> getAll();
+
+	@Query("SELECT COUNT(*) FROM estados");
+	int countStates();
+
+	@Delete
+	void delete(State state);
 }
