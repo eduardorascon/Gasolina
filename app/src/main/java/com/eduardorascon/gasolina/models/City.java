52570bdf7c5438a5eced2954ff71cc2ea2822571
Eduardo Rascon <eduardo.rascon@gmail.com>
2017-11-18 20:35:33 -0600

    Index added to state_id FK.

--- app/src/main/java/com/eduardorascon/gasolina/models/City.java
+++ app/src/main/java/com/eduardorascon/gasolina/models/City.java
@@ -3,9 +3,12 @@ package com.eduardorascon.gasolina.models;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
 
-@Entity(tableName = "ciudades", foreignKeys = @ForeignKey(entity = State.class, parentColumns = "stateId", childColumns = "state_id"))
+@Entity(tableName = "ciudades",
+        indices = @Index("state_id"),
+        foreignKeys = @ForeignKey(entity = State.class, parentColumns = "stateId", childColumns = "state_id"))
 public class City {
     @PrimaryKey(autoGenerate = true)
     public int cityId;
