455ca29f3e2a96328001c214d29b21ce22b02d7b
Eduardo Rascon <eduardo.rascon@gmail.com>
2017-11-20 13:54:20 -0600

    Table configuracion renamed to configuracion_firebase.

--- app/src/main/java/com/eduardorascon/gasolina/models/Setting.java
+++ app/src/main/java/com/eduardorascon/gasolina/models/Setting.java
@@ -4,7 +4,7 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
-@Entity(tableName = "configuracion")
+@Entity(tableName = "configuracion_firebase")
 public class Setting {
     @PrimaryKey(autoGenerate = true)
     public int settingId;
