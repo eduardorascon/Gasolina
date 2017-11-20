6e233196d197b20dd763fc2681b257139500f04c
Eduardo Rascon <eduardo.rascon@gmail.com>
2017-11-20 13:58:42 -0600

    countSettings function added.

--- app/src/main/java/com/eduardorascon/gasolina/models/SettingDao.java
+++ app/src/main/java/com/eduardorascon/gasolina/models/SettingDao.java
@@ -1,4 +1,8 @@
 package com.eduardorascon.gasolina.models;
 
+import android.arch.persistence.room.Query;
+
 public interface SettingDao {
+    @Query("SELECT COUNT(*) FROM configuracion_firebase")
+    public int countSettings();
 }
