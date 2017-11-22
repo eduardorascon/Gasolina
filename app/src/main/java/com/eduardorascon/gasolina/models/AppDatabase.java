ce2b142f3745d203d265455d36a51e49e437385f
eduardorascon <eduardo.rascon@gmail.com>
2017-11-21 19:32:33 -0600

    RoomDatabase instance created with the Singleton Pattern.

--- app/src/main/java/com/eduardorascon/gasolina/models/AppDatabase.java
+++ app/src/main/java/com/eduardorascon/gasolina/models/AppDatabase.java
@@ -7,7 +7,22 @@ import android.arch.persistence.room.TypeConverters;
 @Database(entities = {State.class, City.class}, version = 1, exportSchema = false)
 @TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
-    public abstract StateDao stateDao();
+    private static AppDatabase INSTANCE;
 
+    public abstract StateDao stateDao();
     public abstract CityDao cityDao();
+
+    public static AppDatabase getInMemoryDatabase(Context context) {
+        if(INSTANCE == null) {
+            INSTANCE = Room.inMemoryDatabaseBuilder(context.getApplicationContext(), AppDatabase.class)
+                .allowMainThreadQueries()
+                .build();
+        }
+
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
 }
