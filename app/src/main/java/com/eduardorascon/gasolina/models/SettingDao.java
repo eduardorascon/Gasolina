74eb8a40d64c7c0d4ac4d4e8e71283f9272afae0
eduardorascon <eduardo.rascon@gmail.com>
2017-11-23 18:04:53 -0600

    Insert annotation for inserting settings and Query annotation to get the last setting in database.

--- app/src/main/java/com/eduardorascon/gasolina/models/SettingDao.java
+++ app/src/main/java/com/eduardorascon/gasolina/models/SettingDao.java
@@ -5,4 +5,10 @@ import android.arch.persistence.room.Query;
 public interface SettingDao {
     @Query("SELECT COUNT(*) FROM configuracion_firebase")
     public int countSettings();
+
+    @Query("SELECT * FROM configuracion_firebase ORDER BY setting_id desc LIMIT 1")
+    public Setting getSettings();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void insertSettings(Setting setting);
 }
